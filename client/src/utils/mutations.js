b439ded3b4aea2774a99a45b5998a9a10b348afd
Rajni bala <balatamna16@gmail.com>
2023-09-18 16:10:22 -0400

    SAVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,21 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
