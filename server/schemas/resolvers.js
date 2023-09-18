60479f98ced6e2d1b59b5ee1cef41c8d136e7127
Rajni bala <balatamna16@gmail.com>
2023-09-18 15:38:06 -0400

    query added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,23 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+  Query: {
+   // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+
+};
+
+module.exports = resolvers;
