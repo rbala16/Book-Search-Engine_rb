c49484c16d776163324abcea0c60dffe15e4270e
Rajni bala <balatamna16@gmail.com>
2023-09-21 12:16:46 -0400

    port updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
